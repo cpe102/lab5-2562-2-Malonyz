87378d43573fbfa1384f9e59115c9f9f2a638297
Malonyz <mapmap1234@hotmail.com>
2020-01-07 15:23:33 +0700

    lab5_1

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,20 +2,34 @@
 using namespace std;
 
 int main(){
+	int i=0;
 	int N;
 	cout << "Enter the number of student: ";
 	cin >> N;
 	
 	string name[N];
 	int age[N];
+	while (i<N){
 	cout << "Name of student = ";
+	cin >> name[i];
 	cout << "Age of student = ";
+	cin >> age[i];
+	i++;
+	}
 	
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
+	int x = 0;
+	while (x<N){
+		if (key == age[x]){
+			cout << name[x] <<"\n";
+		}
+		x++;
+	}
+
 	
 
 	
